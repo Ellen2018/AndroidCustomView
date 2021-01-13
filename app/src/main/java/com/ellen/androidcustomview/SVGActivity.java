99e6162c336283f559f26560b3e3d9801906c63e
ClAndEllen <1961075191@qq.com>
2021-01-13 14:18:02 +0800

    SVG实现 & SVG动画的实现 代码比较死板,不一一演示了

---
+++ app/src/main/java/com/ellen/androidcustomview/SVGActivity.java
@@ -0,0 +1,20 @@
+package com.ellen.androidcustomview;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 演示SVG & SVG动画实现
+ * 参考博文:
+ * https://www.jianshu.com/p/0555b8c1d26a
+ * https://www.jianshu.com/p/47b0ae970dd5
+ */
+public class SVGActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
